8aa9ba02cceffc056835f2e8369881c817babb44
raviehasan <ravie.hasan@ui.ac.id>
2023-09-12 20:42:26 +0700

    modified: models.py (__str__ method for unit test)

--- main/models.py
+++ main/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     description = models.TextField(name="description")
     price = models.IntegerField(name="price")
     date_added = models.DateField(auto_now_add=True, name="date_added")
+
+    def __str__(self):
+        return f"Name: {self.name} {self.amount} {self.price} {self.date_added} {self.description}"
