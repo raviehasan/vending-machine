67bcc2aa25fb986be5023cca3af328d79c134557
raviehasan <ravie.hasan@ui.ac.id>
2023-09-12 20:45:01 +0700

    modifies: urls.py (products/ routing)

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, show_products
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('', show_main, name='show_main'), 
+    path('products/', show_products, name='show_product')
 ]
